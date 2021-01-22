0bcf0c1c7f0992b6ec9e3115ad97ec573ba8b130
Vojtěch Tesař <vtesar@centrum.cz>
2021-01-22 20:21:59 +0100

    Základní kostra stránky

---
+++ mainpage/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="resourses/img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../resourses/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Vlaščovka</title>
+</head>
+
+<body>
+    <?php
+    include('../resourses/php/header.php');
+    ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-9">
+                <div class="chat">
+                    <h1>CHAT coming soon...</h1>
+                </div>
+            </div>
+            <div class="sidebar col-3 pr-0">
+
+                <div class="pos-f-t">
+                    <nav class="navbar navbar-dark justify-content-end">
+                        <button class="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </nav>
+                    <div class="collapse show" id="navbarToggleExternalContent">
+                        <div class="p-4">
+                            <h2>Friend list:</h2>
+                            <div class="wrapper">
+                                <button type="button" class="btn btn-primary btn-lg mb-3">Správa přátel <div class="img-box"><img src="login/img/friends.png" alt="friends"></div></button>
+                            </div>
+                            <ul class="list-group">
+                                <li class="list-group-item">User 1</li>
+                                <li class="list-group-item">User 1</li>
+                                <li class="list-group-item">User 1</li>
+                                <li class="list-group-item">User 1</li>
+                                <li class="list-group-item">User 1</li>
+                                <li class="list-group-item">User 1</li>
+                                <li class="list-group-item">User 1</li>
+                                <li class="list-group-item">User 1</li>
+                                <li class="list-group-item">User 1</li>
+                                <li class="list-group-item">User 1</li>
+                                <li class="list-group-item">User 1</li>
+                                <li class="list-group-item">User 1</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+
+
+    </div>
+
+    <?php
+    include('../resourses/php/footer.php');
+    ?>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="resourses/js/app.js"></script>
+</body>
+
+</html>
